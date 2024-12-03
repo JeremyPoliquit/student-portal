eec51974db40cfe7a1a2b414b40c2f083357a55e
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-03 07:39:39 -0800

    input form for student data information

---
+++ app/components/inputForm.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface InputFormProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  name: string
+}
+
+function inputForm({ onChange, value, placeholder, name }: InputFormProps) {
+  return (
+    <input
+      type="text"
+      className="input input-bordered w-full"
+      name={name}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+}
+
+export default inputForm;
